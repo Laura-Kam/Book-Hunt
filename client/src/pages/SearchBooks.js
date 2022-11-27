a0eeb5f6cbf958e3a7a63e1f57549240815f65b7
Laura Kambanis <kambanislaura@gmail.com>
2022-11-27 13:41:11 +0000

    attempt to add SAVE BOOK mutation hook to saved books .js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -12,9 +12,9 @@ import {
 import Auth from "../utils/auth";
 //get rid of this import - use Apollo use Mutation hook to
 
-import { SAVE_BOOK } from "../utils/queries";
+import { gql, useMutation } from "@apollo/client";
 
-// import { SAVE_BOOK } from "../utils/mutations";
+import { SAVE_BOOK } from "../utils/mutations";
 
 import { saveBook, searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
@@ -31,7 +31,6 @@ const SearchBooks = () => {
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
-    localStorage.setSavedBookIds("mySavedBookIds", savedBookIds);
     return () => saveBookIds(savedBookIds);
   });
 
@@ -68,9 +67,13 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
+
+  //attemp to use useMutation hook to execue save_book mutation. keep try catch block.
+
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    const bookToSave = ([saveBook, { data, loading, error }] =
+      useMutation(SAVE_BOOK));
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
